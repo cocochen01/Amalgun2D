9658cd78319ea243891e9ac022a189e3b55154b3
Coco <cocochen12301@gmail.com>
2025-01-20 15:47:13 -0800

    feat: Camera stretches toward cursor location

--- Assets/Scripts/Player/Aim/PlayerAiming.cs
+++ Assets/Scripts/Player/Aim/PlayerAiming.cs
@@ -1,19 +1,27 @@
 using UnityEngine;
+using Amalgun2D.Core;
+using Cinemachine;
 
 namespace Amalgun2D.Player
 {
     public class PlayerAiming : MonoBehaviour
     {
-        private Camera playerCamera;
         public Vector3 mousePosition;
+
+        private float cameraStretchFactor = .3f;
+
+        private float maxOffsetX = 2f;
+        private float maxOffsetY = 2f;
+
+        private CinemachineFramingTransposer framingTransposer;
         private void Start()
         {
-            playerCamera = Camera.main;
+            framingTransposer = GameManager.Instance.virtualCamera.GetCinemachineComponent<CinemachineFramingTransposer>();
         }
-
         private void Update()
         {
-            mousePosition = playerCamera.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition = GameManager.Instance.playerCamera.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0;
 
             Vector3 direction = mousePosition - transform.position;
             direction.z = 0;
@@ -21,6 +29,15 @@ namespace Amalgun2D.Player
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
             transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        }
+
+        private void LateUpdate()
+        {
+            Vector3 offset = (mousePosition - transform.position) * cameraStretchFactor;
+            offset.x = Mathf.Clamp(offset.x, -maxOffsetX, maxOffsetX);
+            offset.y = Mathf.Clamp(offset.y, -maxOffsetY, maxOffsetY);
+            framingTransposer.m_TrackedObjectOffset = new Vector3(offset.x, offset.y, 0);
         }
     } 
 }
